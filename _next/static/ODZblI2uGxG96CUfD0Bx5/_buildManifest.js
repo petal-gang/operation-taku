2709005c6b7547aaebcc003ac3f2d57093327525
ghaleprachan <ghaleprachan@users.noreply.github.com>
2026-05-26 05:39:52 +0000

    deploy: 4c8d325135e0829d08ca9cb1335c7afe24839322

---
+++ _next/static/ODZblI2uGxG96CUfD0Bx5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/operation-taku//_next/:path+",
+        "destination": "/operation-taku/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
